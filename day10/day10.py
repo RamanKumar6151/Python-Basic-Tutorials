97ca371fd922f9260545ac542fa5d7ab26434f8e
Raman <ramankp655@gmail.com>
2024-01-02 14:15:25 +0530

    completed till day 10-CodeWithHarry

---
+++ day10/day10.py
@@ -0,0 +1,6 @@
+# taking user inputs
+# input()
+a=input("enter your name ")
+b=input("enter your name ")
+# print(a)
+print(int(a)+int(b))
---
+++ day9/day9.py
@@ -0,0 +1,10 @@
+# type casting
+a="1"
+# a=1
+b="2"
+# b=2
+# print(a+b)
+print(int(a)+int(b))
+c=1.9
+d=8
+print(type(c+d))
